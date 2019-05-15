1073ee7c3a18fb3ff502c08afc636cbb266aab62
Jason Butz <736696+jbutz@users.noreply.github.com>
2019-05-15 12:00:04 -0400

    groovysdk 2.4.17

--- Formula/groovysdk@2.4.rb
+++ Formula/groovysdk@2.4.rb
@@ -1,13 +1,10 @@
-class Groovysdk < Formula
+class GroovysdkAT24 < Formula
   desc "SDK for Groovy: a Java-based scripting language"
   homepage "http://www.groovy-lang.org"
-  url "https://dl.bintray.com/groovy/maven/apache-groovy-sdk-2.5.4.zip"
-  sha256 "f8e32382ff0b4079e70d47f776363b86ce5ec0ffe56d73608c3ec1dca8d01fc0"
+  url "https://dl.bintray.com/groovy/maven/apache-groovy-sdk-2.4.17.zip"
+  sha256 "7891f9aad6e489f021d60a67257e11c71703b684b3127b3c2caaff778f973189"
 
   bottle :unneeded
-
-  # Groovy 2.5 requires JDK8+ to build and JDK7 is the minimum version of the JRE that we support.
-  depends_on :java => "1.7+"
 
   conflicts_with "groovy", :because => "both install the same binaries"
 
@@ -19,7 +16,7 @@ class Groovysdk < Formula
 
     prefix.install_metafiles
     bin.install Dir["bin/*"]
-    libexec.install "conf", "lib", "src", "doc"
+    libexec.install %w[conf lib embeddable src doc]
     bin.env_script_all_files(libexec+"bin", :GROOVY_HOME => ENV["GROOVY_HOME"])
   end
 
